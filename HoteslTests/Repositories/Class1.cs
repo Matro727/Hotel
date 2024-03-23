990f3a6040aedce9cb1dee1474ab7ff05375fba9
Mitko Mishev <mi6ev2006@gmail.com>
2024-03-24 00:44:53 +0200

    added tests

--- HoteslTests/Repositories/Class1.cs
+++ HoteslTests/Repositories/Class1.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using NUnit.Framework;
+using Hotel.Data.Entities;
+using Hotel.Repositories.Interfaces;
+using Moq;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HotelTests.Repositories
+namespace Hotel.Repositories.Tests
 {
-    internal class Class1
+    [TestFixture]
+    public class RoomRepositoryTests
     {
+        private Mock<IRoomRepository> mockRoomRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            mockRoomRepository = new Mock<IRoomRepository>();
+        }
+
+        [Test]
+        public void GetAll_ReturnsAllRooms()
+        {
+            // Arrange
+            var rooms = new List<Room>
+            {
+                new Room { Id = 1, Number = 101, Type = "Single", Rentability = "taken", Price = 50 },
+                new Room { Id = 2, Number = 102, Type = "Double", Rentability = "taken", Price = 100 }
+            };
+            mockRoomRepository.Setup(repo => repo.GetAll()).Returns(rooms);
+
+            // Act
+            var result = mockRoomRepository.Object.GetAll();
+
+            // Assert
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(rooms[0], result.ElementAt(0));
+            Assert.AreEqual(rooms[1], result.ElementAt(1));
+        }
+
+        [Test]
+        public void Get_ValidId_ReturnsRoom()
+        {
+            // Arrange
+            var roomToReturn = new Room { Id = 1, Number = 101, Type = "Single", Rentability = "taken", Price = 50 };
+            mockRoomRepository.Setup(repo => repo.Get(1)).Returns(roomToReturn);
+
+            // Act
+            var result = mockRoomRepository.Object.Get(1);
+
+            // Assert
+            Assert.AreEqual(roomToReturn, result);
+        }
+
+
     }
 }
