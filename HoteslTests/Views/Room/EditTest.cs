db9402899e9ff0a977ab1d6cadfac79dde1fb463
Mitko Mishev <mi6ev2006@gmail.com>
2024-03-24 12:23:05 +0200

    1 more test done

---
+++ HoteslTests/Views/Room/EditTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+[TestFixture]
+public class RazorViewTests
+{
+    private IWebDriver _driver;
+    private const string BaseUrl = "http://localhost:5000"; 
+
+    [Test]
+    public void Setup()
+    {
+        
+        var options = new ChromeOptions();
+        options.AddArgument("--headless"); 
+        _driver = new ChromeDriver(options);
+    }
+
+    
+}
