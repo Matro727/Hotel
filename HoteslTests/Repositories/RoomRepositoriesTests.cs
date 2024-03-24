0621f2e325b44c9d21e6927e33407720a83e1830
Мартин Джупаров <bulgaria.is.mine@gmail.com>
2024-03-24 02:38:23 +0200

    Tests

---
+++ HoteslTests/Repositories/RoomRepositoriesTests.cs
@@ -0,0 +1,112 @@
+﻿using Hotel.Data;
+using Microsoft.EntityFrameworkCore;
+using Hotel.Repositories;
+using Hotel.Data.Entities;
+
+namespace Hotel.Repositories
+{
+    public class RoomRepositoryTests
+    {
+        private RoomRepository roomRepository;
+
+        private ApplicationContext applicationContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            applicationContext = SetUpApplicationContext();
+
+            roomRepository = new RoomRepository(applicationContext);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            applicationContext.Database.EnsureDeleted();
+        }
+
+        #region Add
+        [Test]
+        public void GivenAroom_WhenAddingAroom_AddsIt()
+        {
+            var room = new Room(1, 101, "single", "Nottaken", 120);
+
+            roomRepository.Add(room);
+
+            var createdroom = applicationContext.Rooms.LastOrDefault();
+
+            Assert.AreEqual(room, createdroom, "room is different than expected");
+        }
+
+        [Test]
+        public void GivenNullroom_WhenAddingAroom_Throws()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => roomRepository.Add(null));
+
+            Assert.AreEqual(
+                "room can't be null!",
+                exception.Message,
+                "Exception message is different than expexted");
+        }
+        #endregion
+
+        #region GetAll
+        [Test]
+        public void WhenGettingAll_ReturnsAllrooms()
+        {
+            var expectedrooms = Seedrooms();
+
+            var rooms = roomRepository.GetAll();
+
+            Assert.AreEqual(expectedrooms, rooms);
+        }
+        #endregion
+
+        #region Get
+        [Test]
+        public void GivenAnExistingId_WhenGettingAroom_ReturnsTheroom()
+        {
+            var expectedrooms = Seedrooms();
+            var expectedroom = expectedrooms.First();
+
+            var room = roomRepository.Get(expectedroom.Id);
+
+            Assert.AreEqual(expectedroom, room, "room is different than expected");
+        }
+
+        [Test]
+        public void GivenNonExistingId_WhenGettingAroom_ReturnsTheroom()
+        {
+            Seedrooms();
+            var nonExistingId = -1;
+
+            var room = roomRepository.Get(nonExistingId);
+
+            Assert.Null(room);
+        }
+        #endregion
+
+        private IEnumerable<Room> Seedrooms()
+        {
+            var rooms = new[]
+            {
+                new Room(1, 101, "single", "Nottaken", 120),
+                new Room(2, 102,  "single", "Nottaken", 120),
+                new Room(3, 103,  "single", "Nottaken", 120)
+            };
+
+            applicationContext.Rooms.AddRange(rooms);
+            applicationContext.SaveChanges();
+
+            return rooms;
+        }
+
+        private ApplicationContext SetUpApplicationContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("UnitTestsDb");
+
+            return new ApplicationContext(options.Options);
+        }
+    }
+}
--- HoteslTests/Services/RoomServiceTest.cs
+++ HoteslTests/Services/RoomServiceTest.cs
@@ -1,42 +1,127 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using Moq;
 using Hotel.Data.Entities;
-using Hotel.Models.Room;
 using Hotel.Repositories.Interfaces;
 using Hotel.Services;
-using Moq;
-using NUnit.Framework;
+using Hotel.Models.Room;
 
-namespace Hotel.Tests.Services
+namespace rooms_Web.Tests.Services
 {
-    [TestFixture]
-    public class RoomServiceTests
+    public class roomServiceTests
     {
-        [Test]
-        public void RoomService_GetAll_ReturnsRoomViewModels()
+        private RoomService roomService;
+
+        private Mock<IRoomRepository> roomRepositoryMock;
+
+        private readonly IEnumerable<Room> roomsInDatabase;
+
+        public roomServiceTests()
         {
-            // Arrange
-            var roomEntities = new List<Room>
+            roomsInDatabase = new[]
             {
-                new Room { Id = 1, Number = 101, Type = "Single", Rentability = "Rentable", Price = 50 },
-                new Room { Id = 2, Number = 102, Type = "Double", Rentability = "Rentable", Price = 75 }
+                new Room(1, 101, "single", "Nottaken", 120),
+                new Room(2, 102, "single", "Nottaken", 120),
+                new Room(3, 103, "single", "Nottaken", 120)
             };
-
-            var roomViewModels = roomEntities.Select(room => new RoomViewModel(room.Id, room.Number, room.Type, room.Rentability, room.Price));
-
-            var roomRepositoryMock = new Mock<IRoomRepository>();
-            roomRepositoryMock.Setup(repo => repo.GetAll()).Returns(roomEntities);
-
-            var roomService = new RoomService(roomRepositoryMock.Object);
-
-            // Act
-            var result = roomService.GetAll();
-
-            // Assert
-            Assert.AreEqual(roomViewModels.Count(), result.Count());
-            Assert.IsTrue(result.All(r => roomViewModels.Any(viewModel => viewModel.Id == r.Id)));
         }
 
-        // Similar tests can be written for other methods such as Add, Delete, Edit, and Get.
+        [SetUp]
+        public void SetUp()
+        {
+            roomRepositoryMock = SetUproomRepositoryMock();
+
+            roomService = new RoomService(roomRepositoryMock.Object);
+        }
+
+        #region Add
+        [Test]
+        public void GivenValidroom_WhenAddingAroom_TheroomIsAdded()
+        {
+
+            var room = new CreateRoomViewModel()
+            {
+                Number = 1,
+                Type = "NotTaken",
+                Price = 132
+            };
+            roomService.Add(room);
+
+            roomRepositoryMock.Verify(
+                mock => mock.Add(It.Is<Room>(roomEntity =>
+                    roomEntity.Number == room.Number &&
+                    roomEntity.Type == room.Type &&
+                    roomEntity.Price == room.Price)),
+                Times.Once);
+        }
+        #endregion
+
+        #region GetAll
+        [Test]
+        public void GivenroomsExist_WhenGettingAllrooms_AllroomsAreReturned()
+        {
+            var rooms = roomService.GetAll();
+
+            Assert.AreEqual(
+                roomsInDatabase.Count(),
+                rooms.Count(),
+                "rooms count different than expected");
+
+            foreach (var roomInDatabase in roomsInDatabase)
+            {
+                var roomExists = rooms.Any(room =>
+                        room.Id == roomInDatabase.Id &&
+                        room.Number == roomInDatabase.Number &&
+                        room.Type == roomInDatabase.Type &&
+                        room.Price == roomInDatabase.Price);
+
+                Assert.True(
+                    roomExists,
+                    $"room with Id {roomInDatabase.Id} doesn't exist");
+            }
+        }
+
+        [Test]
+        public void GivenNoroomsExist_WhenGettingAllrooms_ReturnsEmptyCollection()
+        {
+            roomRepositoryMock
+                .Setup(mock => mock.GetAll())
+                .Returns(new List<Room>());
+
+            var rooms = roomService.GetAll();
+
+            Assert.AreEqual(0, rooms.Count());
+        }
+        #endregion
+
+        #region Get
+        [Test]
+        public void GivenAnExistingId_WhenGettingAroom_ReturnsTheroom()
+        {
+            var expectedroom = roomsInDatabase.First();
+
+            var room = roomService.Get(expectedroom.Id);
+
+            Assert.AreEqual(expectedroom.Id, room.Id);
+            Assert.AreEqual(expectedroom.Number, room.Number);
+            Assert.AreEqual(expectedroom.Type, room.Type);
+            Assert.AreEqual(expectedroom.Price, room.Price);
+        }
+        #endregion
+
+        private Mock<IRoomRepository> SetUproomRepositoryMock()
+        {
+            var roomRepositoryMock = new Mock<IRoomRepository>();
+
+            roomRepositoryMock.Setup(mock => mock.Add(It.IsAny<Room>()));
+
+            roomRepositoryMock
+                .Setup(mock => mock.GetAll())
+                .Returns(roomsInDatabase);
+
+            roomRepositoryMock
+                .Setup(mock => mock.Get(roomsInDatabase.First().Id))
+                .Returns(roomsInDatabase.First());
+
+            return roomRepositoryMock;
+        }
     }
 }
